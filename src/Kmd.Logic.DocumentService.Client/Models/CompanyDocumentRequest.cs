01705cac5cd9d58461bad7d44a56dd1a6bf18f0f
Swapnil Bhilwar <ITE@kmd.dk>
2022-06-03 12:00:10 +0530

    Added missing parameter nodes

--- src/Kmd.Logic.DocumentService.Client/Models/CompanyDocumentRequest.cs
+++ src/Kmd.Logic.DocumentService.Client/Models/CompanyDocumentRequest.cs
@@ -25,11 +25,20 @@ namespace Kmd.Logic.DocumentService.Client.Models
         /// <summary>
         /// Initializes a new instance of the CompanyDocumentRequest class.
         /// </summary>
+        /// <param name="documentConfigurationId"></param>
+        /// <param name="cvrs"></param>
+        /// <param name="id"></param>
         /// <param name="companyDocumentType">Possible values include:
         /// 'Document', 'DigitalPostCoverLetter',
         /// 'SnailMailCoverLetter'</param>
+        /// <param name="documentUrl"></param>
+        /// <param name="retentionPeriodInDays"></param>
         /// <param name="status">Possible values include: 'InProgress',
         /// 'Completed', 'Failed'</param>
+        /// <param name="fileName"></param>
+        /// <param name="documentName"></param>
+        /// <param name="sender"></param>
+        /// <param name="documentComment"></param>
         public CompanyDocumentRequest(System.Guid documentConfigurationId, IList<string> cvrs, System.Guid? id = default(System.Guid?), string companyDocumentType = default(string), string documentUrl = default(string), int? retentionPeriodInDays = default(int?), string status = default(string), string fileName = default(string), string documentName = default(string), string sender = default(string), string documentComment = default(string))
         {
             Id = id;
